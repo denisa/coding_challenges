facd9377a03bf31fc0b120517637acb841b38e25
Denis N. Antonioli <denisa@sunrunhome.com>
2014-02-11 07:45:03 -0800

    1st draft

---
+++ src/main/java/li/antonio/code_eval/find_min/FindMin.java
@@ -0,0 +1,100 @@
+package li.antonio.code_eval.find_min;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+public class FindMin {
+
+    private static final Pattern COMA_SEPARATED = Pattern.compile(",");
+
+    public static void main(final String[] argv) throws IOException {
+        for (final String s : Files.readAllLines(new File(argv[0]).toPath(), Charset.defaultCharset())) {
+            final String[] split = COMA_SEPARATED.split(s);
+            final int n = Integer.parseInt(split[0]);
+            final int k = Integer.parseInt(split[1]);
+            final int a = Integer.parseInt(split[2]);
+            final int b = Integer.parseInt(split[3]);
+            final int c = Integer.parseInt(split[4]);
+            final int r = Integer.parseInt(split[5]);
+            System.out.println(find_min(n, k, a, b, c, r));
+        }
+    }
+
+    static int find_min(final int n, final int k, final int a, final int b, final int c, final int r) {
+        // todo (Denis) Math.min(k, n) ?
+        final int[] m = seed(k, a, b, c, r);
+        final int[] sorted_m = m.clone();
+        Arrays.sort(sorted_m);
+
+        System.out.println(Arrays.toString(m));
+        System.out.println(Arrays.toString(sorted_m));
+
+        for (int i = k; i < n; i++) {
+            final int mnc = minimumNotContained(sorted_m);
+            final int removed = left_shift(m, mnc);
+            replace(sorted_m, removed, mnc);
+            System.out.println(i + ":\n");
+            System.out.println(Arrays.toString(m));
+            System.out.println(Arrays.toString(sorted_m));
+        }
+        return m[m.length - 1];
+    }
+
+    static int[] seed(final int k, final int a, final int b, final int c, final int r) {
+        final int[] m = new int[k];
+        m[0] = a;
+        for (int i = 1; i < k; i++) {
+            m[i] = (b * m[i - 1] + c) % r;
+        }
+        return m;
+    }
+
+    static int minimumNotContained(final int[] m) {
+        if (m[0] > 0) {
+            return 0;
+        }
+        for (int i = 1; i < m.length; i++) {
+            final int candidate = m[i - 1] + 1;
+            if (m[i] > candidate) {
+                return candidate;
+            }
+        }
+        return m[m.length - 1] + 1;
+    }
+
+    static int left_shift(final int[] m, final int added) {
+        final int removed = m[0];
+        System.arraycopy(m, 1, m, 0, m.length - 1);
+        m[m.length - 1] = added;
+        return removed;
+    }
+
+    static void replace(final int[] m, final int removed, final int added) {
+        if (removed == added) {
+            return;
+        }
+
+        final int removalIndex = Arrays.binarySearch(m, removed);
+        if (removalIndex < m.length - 1) {
+            System.arraycopy(m, removalIndex + 1, m, removalIndex, m.length - removalIndex - 1);
+        }
+        if (added >= m[m.length - 1]) {
+            m[m.length - 1] = added;
+            return;
+        }
+        final int insertIndex = Arrays.binarySearch(m, added);
+        if (insertIndex < 0) {
+            final int iIndex = -(insertIndex + 1);
+            System.arraycopy(m, iIndex, m, iIndex + 1, m.length - iIndex - 1);
+            m[iIndex] = added;
+        } else {
+            System.arraycopy(m, insertIndex, m, insertIndex + 1, m.length - insertIndex - 1);
+            m[insertIndex] = added;
+        }
+    }
+
+}
---
+++ src/test/java/li/antonio/code_eval/find_min/FindMinTest.java
@@ -0,0 +1,93 @@
+package li.antonio.code_eval.find_min;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * https://www.codeeval.com/browse/85/
+ */
+public class FindMinTest {
+
+
+    private static int[] fill(final int[] m, final int k) {
+        for (int i = k; i < m.length; i++) {
+
+            final Set<Integer> set = new HashSet<>();
+            for (int j = i - k; j < i; j++) {
+                set.add(m[j]);
+            }
+
+            final Integer[] integers = set.toArray(new Integer[set.size()]);
+            Arrays.sort(integers);
+//            System.out.println("all integers: " + Arrays.asList(integers));
+            m[i] = smallestMissingInt(integers);
+//            System.out.println("Added "+ m[i]);
+        }
+        return m;
+    }
+
+    private static int smallestMissingInt(final Integer[] integers) {
+        if (integers[0] > 0) {
+            return 0;
+        }
+        for (int j = 1; j < integers.length; j++) {
+            if (integers[j] - integers[j - 1] > 1) {
+                return integers[j - 1] + 1;
+            }
+        }
+        if (integers[integers.length - 1] - integers[integers.length - 2] > 1) {
+            return integers[integers.length - 2] + 1;
+        }
+        return integers[integers.length - 1] + 1;
+    }
+
+    private static int print(final int[] m) {
+        return m[m.length - 1];
+    }
+
+    @Test
+    public void find_min() {
+        Assert.assertEquals(26, FindMin.find_min(78, 51, 3, 5, 5, 51230));
+        Assert.assertEquals(34, FindMin.find_min(186, 75, 68, 16, 539, 312));
+        Assert.assertEquals(1, FindMin.find_min(137, 135, 48, 17, 461, 512));
+        Assert.assertEquals(6, FindMin.find_min(98, 22, 6, 30, 524, 100));
+        Assert.assertEquals(12, FindMin.find_min(46, 18, 7, 11, 9, 46));
+    }
+
+    @Test
+    public void minimumNotContained() {
+        Assert.assertEquals(0, FindMin.minimumNotContained(new int[] { 1, 2 }));
+        Assert.assertEquals(2, FindMin.minimumNotContained(new int[] { 0, 1 }));
+        Assert.assertEquals(1, FindMin.minimumNotContained(new int[] { 0, 2 }));
+    }
+
+    @Test
+    public void left_shift() {
+        final int[] m = { 0, 1, 2 };
+        Assert.assertEquals(0, FindMin.left_shift(m, 3));
+        Assert.assertArrayEquals(new int[] { 1, 2, 3 }, m);
+    }
+
+    @Test
+    public void replace() {
+        {
+            final int[] m = { 0, 1, 2 };
+            FindMin.replace(m, 1, 3);
+            Assert.assertArrayEquals(new int[] { 0, 2, 3 }, m);
+        }
+        {
+            final int[] m = { 1, 2, 3 };
+            FindMin.replace(m, 1, 0);
+            Assert.assertArrayEquals(new int[] { 0, 2, 3 }, m);
+        }
+        {
+            final int[] m = { 0, 2, 3 };
+            FindMin.replace(m, 2, 1);
+            Assert.assertArrayEquals(new int[] { 0, 1, 3 }, m);
+        }
+    }
+}
